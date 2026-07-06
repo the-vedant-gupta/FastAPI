9e29ff30fe362b16ba7acef060f0bd08921fe0e9
the-vedant-gupta <vedantgupta8784@gmai.com>
2026-07-06 23:33:48 +0530

    till Query Parameters

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"data": {"name": "Vedant"}}
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    # only get 10 published blogs
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} blogs from the db"}
 
 
-@app.get("/about")
-def about():
-    return {"data": {"about page"}}
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+
+@app.get("/blog/{id}")
+def show(id: int):
+    # fetch blog with id = id
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id, limit=10):
+    # fetch comments of blog with id = id
+    return limit
+    return {"data": {"1", "2"}}
