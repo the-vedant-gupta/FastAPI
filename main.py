d44fb41f2138454af12b53740eb82590fed23c40
the-vedant-gupta <vedantgupta8784@gmai.com>
2026-07-08 00:40:37 +0530

    Till request body

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
 
 @app.get("/blog")
-def index(limit=10, published: bool = True, sort: Optional[str] = None):
+def index(limit=10, published: bool = True, sort: str | None = None):
     # only get 10 published blogs
     if published:
         return {"data": f"{limit} published blogs from the db"}
@@ -25,7 +27,22 @@ def show(id: int):
 
 
 @app.get("/blog/{id}/comments")
-def comments(id, limit=10):
+def comments(id, limit=100):
     # fetch comments of blog with id = id
     return limit
     return {"data": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: bool | None = None
+
+
+@app.post("/blog")
+def create_blog(blog: Blog):
+    return {"data": f"Blog is created with title as {blog.title} "}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=9000)
