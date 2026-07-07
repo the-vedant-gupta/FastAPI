033e19da016d5f597ed28e50542ed7837096cc2e
the-vedant-gupta <vedantgupta8784@gmai.com>
2026-07-08 00:41:46 +0530

    till request body

--- main.py
+++ main.py
@@ -44,5 +44,5 @@ def create_blog(blog: Blog):
     return {"data": f"Blog is created with title as {blog.title} "}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="127.0.0.1", port=9000)
